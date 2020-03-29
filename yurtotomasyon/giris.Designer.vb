d3d42477e6d9f5c8e2eebb93614135e410086e2c
ZIEatBabysZ <34131658+ZIEatBabysZ@users.noreply.github.com>
2020-03-29 23:54:52 +0300

    daha da son
    son

--- yurtotomasyon/giris.Designer.vb
+++ yurtotomasyon/giris.Designer.vb
@@ -34,7 +34,7 @@ Partial Class giris
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Century Gothic", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.ForeColor = System.Drawing.Color.Transparent
+        Me.Label1.ForeColor = System.Drawing.Color.Black
         Me.Label1.Location = New System.Drawing.Point(182, 31)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(424, 58)
