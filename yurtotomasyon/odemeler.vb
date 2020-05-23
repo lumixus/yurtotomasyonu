4e85e14590741907fe0b400ff04657ac93e03bb2
morbeyus <emre34er@gmail.com>
2020-05-23 23:56:46 +0300

    ogrenciVEodeme

--- yurtotomasyon/odemeler.vb
+++ yurtotomasyon/odemeler.vb
@@ -1,8 +1,26 @@
 ﻿Public Class odemeler
     Dim con As New Class1
-    Private Sub odemeler_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Private Sub odemeler_Load(sender As Object, e As EventArgs) Handles MyBase.Load, DataGridView1.Click
         con.openCon()
         DataGridView1.DataSource = con.getOdemeler()
         con.closeCon()
+    End Sub
+
+
+    Private Sub DataGridView1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
+
+        If IsDBNull(DataGridView1.CurrentRow.Cells(0).Value) Then
+            MessageBox.Show("Dolu olan bir satır seçilmelidir.")
+        Else
+            TextBox1.Text = CStr(DataGridView1.CurrentRow.Cells(0).Value)
+            TextBox2.Text = CStr(DataGridView1.CurrentRow.Cells(1).Value)
+            TextBox3.Text = CStr(DataGridView1.CurrentRow.Cells(4).Value)
+            TextBox4.Text = CStr(DataGridView1.CurrentRow.Cells(3).Value)
+            TextBox5.Text = CStr(DataGridView1.CurrentRow.Cells(5).Value)
+            DateTimePicker2.Value = CStr(DataGridView1.CurrentRow.Cells(2).Value)
+
+        End If
+
+
     End Sub
 End Class
