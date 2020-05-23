624feb54d33a9a95835864469a5a4edb3493d635
morbeyus <emre34er@gmail.com>
2020-05-24 00:09:51 +0300

    odemelerV2

--- yurtotomasyon/odemeler.vb
+++ yurtotomasyon/odemeler.vb
@@ -1,6 +1,6 @@
 ﻿Public Class odemeler
     Dim con As New Class1
-    Private Sub odemeler_Load(sender As Object, e As EventArgs) Handles MyBase.Load, DataGridView1.Click
+    Private Sub odemeler_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         con.openCon()
         DataGridView1.DataSource = con.getOdemeler()
         con.closeCon()
