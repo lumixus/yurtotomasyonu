751549194f7e58484e39287fe59beda28638daf9
helloimzeus <44981267+helloimzeus@users.noreply.github.com>
2020-05-24 23:41:51 +0300

    odemeler

--- yurtotomasyon/odemeler.vb
+++ yurtotomasyon/odemeler.vb
@@ -32,4 +32,40 @@
 
 
     End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim id, ad, odmik, odcekmik, kalmik As String
+        Dim tarih As DateTime
+        DataGridView1.CurrentRow.Cells(0).Value = TextBox1.Text
+        DataGridView1.CurrentRow.Cells(1).Value = TextBox2.Text
+        DataGridView1.CurrentRow.Cells(3).Value = TextBox4.Text
+        DataGridView1.CurrentRow.Cells(5).Value = TextBox5.Text
+        DataGridView1.CurrentRow.Cells(4).Value = TextBox3.Text
+        DataGridView1.CurrentRow.Cells(2).Value = DateTimePicker2.Value
+        id = DataGridView1.CurrentRow.Cells(0).Value.ToString()
+        ad = DataGridView1.CurrentRow.Cells(1).Value.ToString()
+        odmik = DataGridView1.CurrentRow.Cells(3).Value.ToString()
+        odcekmik = DataGridView1.CurrentRow.Cells(4).Value.ToString()
+        kalmik = DataGridView1.CurrentRow.Cells(5).Value.ToString()
+        tarih = DataGridView1.CurrentRow.Cells(2).Value.ToString()
+
+        tarih = Convert.ToDateTime(DataGridView1.CurrentRow.Cells(13).Value.ToString())
+        con.openCon()
+        con.updateOdemeler(id, ad, odmik, odcekmik, kalmik, tarih)
+        Dim dtable As DataTable = con.getOdemeler()
+        dtable.Clear()
+        DataGridView1.DataSource = dtable
+        DataGridView1.DataSource = con.getOdemeler()
+        con.closeCon()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        con.openCon()
+        con.deleteVeri("sonodemeler", CStr(DataGridView1.CurrentRow.Cells(0).Value))
+        Dim dtable As DataTable = con.getOdalar()
+        dtable.Clear()
+        DataGridView1.DataSource = dtable
+        DataGridView1.DataSource = con.getOdemeler()
+        con.closeCon()
+    End Sub
 End Class
